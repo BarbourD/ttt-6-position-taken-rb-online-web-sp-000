d5d7cb7b100eaeb83e88407954144150cd3d321c
David Barbour <danceguy303@gmail.com>
2020-08-24 03:46:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+index = 0
+
 def position_taken?(board, index)
- board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
- index = 0
  if board[index_number] == " "
  elsif board[index_number] == ""
- elsif board[index_number] != " "
+ else board[index_number] != " "
 end
